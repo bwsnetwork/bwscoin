b5568cf26e3966e68aa8ec64646a45ba51c28c4c
Patrick Gerzanics <patrick.gerzanics@upandrunningsoftware.com>
2022-01-24 14:34:44 -0600

    Migrated to Ubuntu 20.04 and fixed dependency issues with bwscoin changes and private repository scenario.

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -z "$GITHUB_ACCESS_TOKEN" ]
+then
+  echo "Please define GITHUB_ACCESS_TOKEN to download source archive from private repository."
+  exit
+fi
+
 if [ "$#" -ne 2 ]
 then
     echo "Usage: build.sh <version> <branch>"
@@ -7,7 +13,7 @@ then
 fi
 CWD=$(dirname "$(readlink -f $0)") && cd $CWD && \
 echo $CWD && \
-docker build --rm -t bwscoin-build -f Dockerfile.build . && \
+docker build --network=host --rm -t bwscoin-build -f Dockerfile.build . && \
 rm -f *.deb && \
-docker run -e VERSION=$1 -e BRANCH=$2 --rm --volume $CWD:/bwscoin -i -t bwscoin-build && \
-docker build --rm -t $2:$1 -f Dockerfile .
+docker run --network=host -e GITHUB_ACCESS_TOKEN=$GITHUB_ACCESS_TOKEN -e VERSION=$1 -e BRANCH=$2 --rm --volume $CWD:/bwscoin -i -t bwscoin-build && \
+docker build --network=host --rm -t $2:$1 -f Dockerfile .
