88039495cb2160ed128b58bef8fb0a3e08d3e036
Patrick Gerzanics <patrick.gerzanics@upandrunningsoftware.com>
2022-04-15 11:25:08 -0500

    Removed github token requirement since now public.

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ -z "$GITHUB_ACCESS_TOKEN" ]
-then
-  echo "Please define GITHUB_ACCESS_TOKEN to download source archive from private repository."
-  exit
-fi
-
 if [ "$#" -ne 2 ]
 then
     echo "Usage: build.sh <version> <branch>"
@@ -15,5 +9,5 @@ CWD=$(dirname "$(readlink -f $0)") && cd $CWD && \
 echo $CWD && \
 docker build --network=host --rm -t bwscoin-build -f Dockerfile.build . && \
 rm -f *.deb && \
-docker run --network=host -e GITHUB_ACCESS_TOKEN=$GITHUB_ACCESS_TOKEN -e VERSION=$1 -e BRANCH=$2 --rm --volume $CWD:/bwscoin -i -t bwscoin-build && \
+docker run --network=host -e VERSION=$1 -e BRANCH=$2 --rm --volume $CWD:/bwscoin -i -t bwscoin-build && \
 docker build --network=host --rm -t $2:$1 -f Dockerfile .
